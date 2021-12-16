6acf553140d7683f4055bf8088d6e43c99cbfe9e
Joseph Macalinao <jhmac121101@gmail.com>
2021-12-15 20:36:55 -0700

    all 1 file

--- Main/casino.py
+++ Main/casino.py
@@ -1,8 +1,9 @@
 import random
+import time
 
 cards = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
-        6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10,
-        11, 11, 11, 11, 12, 12, 12, 12]
+6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10,
+11, 11, 11, 11, 12, 12, 12, 12]
 
 
 def twenty_one():
@@ -19,45 +20,91 @@ def twenty_one():
             print(total)
             if total > 21:
                 print("Sorry, you lose...")
-                wager(wage)
+                wager(wage, False)
                 main()
             elif total == 21:
                 print("Congratulations. You won!")
                 wager(wage)
+                main()
     for i in range(3):
         house += random.choice(cards)
         print(f'House: {house}')
     if house > 21:
         print("Congratulations, you won!")
-        wager(wage)#figure out how to pay player
+        wager(wage)
+        main()
     elif house == 21 and total != 21:
         print('Sorry, you lose...')
+        wager(wage, False)
+        main()
     elif house < total:
         print('Congratulations, you won!')
+        wager(wage)
+        main()
     elif house > total:
         print('Sorry, you lose...')
+        wager(wage)
+        main()
     elif house == total:
         print("Tie, go again!")
         twenty_one()
 
+
 def coin():
-    pass
+    hot = True
+    while hot:
+        h_o_t = input("Heads or Tails [H/T]\n")
+        if h_o_t.upper() not in ['H', 'T']:
+            print("Input must be H or T")
+        else:
+            break
+    wage = int(input("How much would you like to bet?"))
+    flip = random.randint(1, 2)
+    print("Flipping...")
+    time.sleep(4)
+    if flip == 1:
+        print("It's heads")
+    else:
+        print("It's tails")
+    if flip == 1 and h_o_t.upper() == 'H':
+        print('Congrats, you won!')
+        wager(wage)
+        main()
+    elif flip == 1 and h_o_t.upper() == 'T':
+        print('Sorry, you lose...')
+        wager(wage, False)
+        main()
+    elif flip == 2 and h_o_t.upper() == 'T':
+        print('Congrats, you won!')
+        wager(wage)
+        main()
+    elif flip == 2 and h_o_t.upper() == 'H':
+        print('Sorry, you lose...')
+        wager(wage, False)
+        main()
 
 
 def roulette():
     pass
 
 
-def wager(bet: float):
+def wager(bet: float, win=True):
     '''
     used in all games to used for the winnings and the betting
     :param bet: how much they are betting
     :return:
     '''
-    pass
+    if win:
+        print(f'You won ${bet * 2}')
+        return bet * 2
+    else:
+        print(f'You lose all your bet')
+        return 0
+
 
 
 def main():
+    time.sleep(3)
     games = ['21', 'C', 'R']
     print("Welcome to Macalinao Chips, the best fake online Python-based Casino ever,  "
           "\nhow may we serve you today?")
